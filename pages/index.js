110e0fa990f3641e239ee585cd37e0933ddc9093
Bruna Viana <94561517+Brunavtm@users.noreply.github.com>
2026-08-27 20:41:42 +0000

    Modifica página inicial.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Iza, se estiver vendo dá uma risadinha. </h1>
+    return <h1>Mozi, se estiver vendo isso me manda um coraçãozinho no whats. Te amo. </h1>
 }
 
 export default Home;
