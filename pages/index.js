c50baef4fd7869b2d38fbee1a48541ad360a27cc
Bruna Viana <94561517+Brunavtm@users.noreply.github.com>
2026-09-01 02:11:40 +0000

    adiciona scripts `lint:check`e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 function Home() {
-    return <h1>Mozi, se estiver vendo isso me manda um coraçãozinho no whats. Te amo. </h1>
+  return (
+    <h1>
+      Mozi, se estiver vendo isso me manda um coraçãozinho no whats. Te amo);
+      .{" "}
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("Teste");
 }
 
 export default Home;
